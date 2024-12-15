c61ba009579abbb7b31f665d7a43c78f1122ef9f
Ghaliah <ghalia.banouna@gmail.com>
2024-12-15 19:01:13 +0300

    Adding models to admin

--- SkillExchange/exchangers/admin.py
+++ SkillExchange/exchangers/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Exchanger, Request
 
-# Register your models here.
+admin.site.register(Exchanger)
+admin.site.register(Request)
--- SkillExchange/main/admin.py
+++ SkillExchange/main/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Contact, Plan, Subscription
 
-# Register your models here.
+admin.site.register(Contact)
+admin.site.register(Plan)
+admin.site.register(Subscription)

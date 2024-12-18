08f8301dff3fe26ecda1050cd541bf2f2bbcabb8
Ghaliah <ghalia.banouna@gmail.com>
2024-12-18 14:23:10 +0300

    Minor modifications in skills app

--- SkillExchange/skills/models.py
+++ SkillExchange/skills/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Skill(models.Model):
     name = models.CharField(max_length=255)  
     image = models.ImageField(upload_to='images/skills/', blank=True, null=True)
-    description = models.TextField(blank=True, null=True)
+    description = models.TextField(max_length=600)
 
     def __str__(self):
         return self.name
